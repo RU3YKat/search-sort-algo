107b9a8128f659365fe70d2f8e5e385743bbbb0b
insomniKat <ru3ykat@gmail.com>
2021-12-12 16:37:34 -0500

    quick algo to test speed and sort fncs

---
+++ index.js
@@ -0,0 +1,59 @@
+const Benchmark = require('benchmark');
+// const { bubbleSort, quickSort } = require('./sort');
+
+// const numbers = [];
+// for (let i = 0; i < 10000; i++) {
+//   numbers.push(Math.floor(Math.random() * 10000) + 1);
+// }
+
+// const numbers = [];
+// for (let i = 1; i <= 5000; i++) {
+//   numbers.push(i);
+// }
+
+// const numbers = [];
+// for (let i = 0; i < 40000; i++) {
+//   numbers.push(Math.floor(Math.random() * 10000) + 1);
+// }
+
+// const suite = new Benchmark.Suite;
+
+// suite
+//     .add('quick sort', function() {
+//         const testArray = [...numbers];
+
+//         quickSort(testArray);
+//     })
+//     .add('js sort', function() {
+//         const testArray = [...numbers];
+
+//         // benchmark the built-in sort method
+//         testArray.sort((a, b) => {
+//         return a - b;
+//         });
+//     })
+//     .on('complete', function() {
+//         this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
+//     })
+//     .run();
+
+const { mostDuplicates, optimizedDuplicates } = require('./dupes');
+
+const numbers = [];
+for (let i = 0; i < 10000; i++) {
+  numbers.push(Math.floor(Math.random() * 10000) + 1);
+}
+
+const suite = new Benchmark.Suite;
+
+suite
+    .add('duplicates test', function() {
+        mostDuplicates(numbers);
+    })
+    .add('optimized duplicates test', function() {
+        optimizedDuplicates(numbers);
+    })
+    .on('complete', function() {
+        this.forEach(result => console.log(`${result.name} averaged ${result.stats.mean*1000} milliseconds.`));
+    })
+    .run();
